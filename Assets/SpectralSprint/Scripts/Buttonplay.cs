cd1eceac5ef1845aedb6b0f06ed3da73297a8814
B09S106est <B09S106est@MEDAULAS.UPB>
2023-08-16 13:38:31 -0500

    post class

---
+++ Assets/SpectralSprint/Scripts/Buttonplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using corgi
+
+public class Buttonplay : MonoBehaviour
+{
+    public void OnPlay()
+    {
+        //MMSceneLoadingManager
+        //https://corgi-engine-docs.moremountains.com/scenes.html
+
+        //Criterios de evaluacion. //hay que actualizar los diseños de nivel, donde van a ir los obstaculos, monedas, enemigos.
+        //
+    }
+}
