bb8a0d5ff146d2bd2d577fc0233881dbac77625c
derethar <derethar@hotmail.com>
2023-08-22 23:21:49 -0500

    Level1+MainMenuComplete

--- Assets/SpectralSprint/Scripts/Buttonplay.cs
+++ Assets/SpectralSprint/Scripts/Buttonplay.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using corgi
+using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttonplay : MonoBehaviour
 {
-    public void OnPlay()
-    {
-        //MMSceneLoadingManager
-        //https://corgi-engine-docs.moremountains.com/scenes.html
+    /// the exact name of the target level
+    [Tooltip("the exact name of the target level")]
+    public string LevelName;
+    /// whether or not changing level should trigger a fade
+    [Tooltip("whether or not changing level should trigger a fade")]
+    public bool Fade = true;
+    /// whether or not changing level should trigger a save event
+    [Tooltip("whether or not changing level should trigger a save event")]
+    public bool Save = true;
 
-        //Criterios de evaluacion. //hay que actualizar los diseños de nivel, donde van a ir los obstaculos, monedas, enemigos.
-        //
+    /// <summary>
+    /// Loads the level specified in the inspector
+    /// </summary>
+    public virtual void GoToLevel()
+    {
+        LevelManager.Instance.GotoLevel(LevelName, Fade, Save);
+    }
+
+    /// <summary>
+    /// Restarts the current level
+    /// </summary>
+    public virtual void RestartLevel()
+    {
+        LevelManager.Instance.GotoLevel(SceneManager.GetActiveScene().name, Fade, Save);
+        // we trigger an unPause event for the GameManager (and potentially other classes)
+        //CorgiEngineEvent.Trigger(CorgiEngineEventTypes.UnPause);
+        //LoadingSceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
