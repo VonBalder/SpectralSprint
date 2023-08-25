179f0e067effc0480d644b411b61379e338070ea
derethar <derethar@hotmail.com>
2023-08-25 08:57:25 -0500

    Randomizer and coins added

---
+++ Assets/SpectralSprint/Scripts/MirrorVictory.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+public class MirrorVictory : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Mirrors;
+    [SerializeField] private int NumMirrors;
+    public string LevelName;
+    [SerializeField] private string TagMirror = ("CoinCount");
+    private void Update()
+    {
+        MirrorActivation();
+        MirrorV();
+    }
+    private void MirrorActivation()
+    {
+       Mirrors = GameObject.FindGameObjectsWithTag(TagMirror);
+    }
+    private void MirrorV()
+    {
+        NumMirrors = Mirrors.Length;
+
+        if (NumMirrors == 0 ) 
+        {
+            NextLevel();
+        }
+    }
+    private void NextLevel()
+    {
+        MMSceneLoadingManager.LoadScene(LevelName);
+    }
+
+    //el script se preguntara cuantos espejos hay de manera constante (MirrorActivation) y se preguntara el numero exacto (MirrorV)
+    //Cuando haga eso tambien se preguntara si el numero de espejos es 0, si el numero es 0, avanza al siguiente nivel
+    //felicidades lograste la victoria espejo
+}
