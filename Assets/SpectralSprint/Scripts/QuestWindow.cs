b6236563f83cffccfab46270277136c6c43c0759
VonBaldr <derethar@hotmail.com>
2023-10-13 11:10:16 -0500

    Entrega3

--- Assets/SpectralSprint/Scripts/QuestWindow.cs
+++ Assets/SpectralSprint/Scripts/QuestWindow.cs
@@ -7,28 +7,32 @@ public class QuestWindow : MonoBehaviour
 {
     [SerializeField]
     private GameObject window;
+    [SerializeField]
+    private int closingtime = 3;
 
     private void Start()
     {
-        StartCoroutine(OpenWindow(5));
+        StartCoroutine(OpenWindow(2));
     }
 
-    public void OnContinue()
+    public void OnContinuar()
     {
-        CloseWindow();
+        StartCoroutine(CloseWindow(0));
     }
-    void CloseWindow()
-    {
-        window.SetActive(false);
-        GameManager.Instance.Pause(PauseMethods.NoPauseMenu);
-    }
-
     IEnumerator OpenWindow(int sec)
     {
         yield return new WaitForSecondsRealtime(sec);
         window.SetActive(true);
         GameManager.Instance.Pause(PauseMethods.NoPauseMenu);
+        StartCoroutine(CloseWindow(closingtime));
     }
 
+    IEnumerator CloseWindow(int sec)
+    {
+        yield return new WaitForSecondsRealtime(sec);
+        window.SetActive(false);
+        GameManager.Instance.Pause(PauseMethods.NoPauseMenu);
+        
+    }
 
 }
